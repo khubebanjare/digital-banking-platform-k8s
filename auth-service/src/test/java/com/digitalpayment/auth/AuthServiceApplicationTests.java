d503e7ef496eedf05206e31026237a40eb93014e
khubebanjare <info.khube@gmail.com>
2026-05-31 11:42:48 +0530

    Initialize multi-module Spring Boot project structure

---
+++ auth-service/src/test/java/com/digitalpayment/auth/AuthServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.digitalpayment.auth;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuthServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
---
+++ config-server/src/test/java/com/digitalpayment/config/ConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.digitalpayment.config;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
